b965bbe869ba3d21de62a55b6a06bce99aec7985
Roberto Piva <r.piva@nemesix.it>
2021-06-09 09:49:22 +0200

    Corretta versione e ripristinato try nello script di test

--- example/main.dart
+++ example/main.dart
@@ -5,16 +5,16 @@ import 'package:xml_rpc/client.dart' as xml_rpc;
 
 void main() async {
   final url = Uri.parse('https://api.flickr.com/services/xmlrpc');
-  // try {
-  var result = await xml_rpc.call(
-    url,
-    'flickr.panda.getList',
-    [
-      {'api_key': 'yourApiKey'}
-    ],
-  );
-  print(result);
-  // } catch (e) {
-  //   print(e);
-  // }
+  try {
+    var result = await xml_rpc.call(
+      url,
+      'flickr.panda.getList',
+      [
+        {'api_key': 'yourApiKey'}
+      ],
+    );
+    print(result);
+  } catch (e) {
+    print(e);
+  }
 }
